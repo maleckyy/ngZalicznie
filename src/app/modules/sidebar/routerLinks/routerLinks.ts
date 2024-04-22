70133a9e4f0153b8dabbb49ca2e6e38698e0860b
maleckyy <kupferpatryk@gmail.com>
2024-04-22 19:52:12 +0200

    sidebar

---
+++ src/app/modules/sidebar/routerLinks/routerLinks.ts
@@ -0,0 +1,22 @@
+export const Links = [
+  {
+    path: "",
+    icon: "desktop",
+    name: "Home"
+  },
+  {
+    path: "/employee-list",
+    icon: "team",
+    name: "Employees list"
+  },
+  {
+    path: "/employee-new",
+    icon: "user-add",
+    name: "Add employee"
+  },
+  {
+    path: "/project-list",
+    icon: "project",
+    name: "Projets list"
+  },
+]
